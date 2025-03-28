2a3155616a48d8ce6111d3d5a98852acddf9f4ac
yemiwebby <yemiwebby@gmail.com>
2025-03-28 07:13:21 +0000

    update route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const routes = require("./routes");
 // App
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("App is up and running!");
+});
+
 app.use("/", routes);
 
 module.exports = app;
